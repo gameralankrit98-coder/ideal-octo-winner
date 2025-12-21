9921f9efe4833a5417f74752b030dc8443816c7f
KRIT98 <gameralankrit98@gmail.com>
2025-12-21 19:21:32 +0530

    Update >,<,= calculator.py

--- >,<,= calculator.py
+++ >,<,= calculator.py
@@ -1,1 +1,32 @@
-print("hello world")
+import time
+
+print ("hey 👋 world")
+
+time.sleep(1)
+
+print ("today we are gonna calculating the place value means the amount of the two numbers you are about to enter 😄")
+
+time.sleep(1.5)
+
+print ("so let's start ☺️")
+
+time.sleep(0.7)
+
+a=int(input("first number - "))
+b=int(input("second\last number - "))
+
+if a==b;
+ print ("so you are trynna trick me no 🙂‍↔️ chance " ,a, "and" ,b, "are same")
+
+elif a<b;
+ print ("obviously" ,a, "is smaller than" ,b,)
+
+elif a>b;
+ print (a, "is larger than" ,b, "😄")
+
+else ;
+ print ("number please 😭🥺 when you haven't entered a single number how could I calculate ")
+
+time.sleep(2)
+
+print ("aaaahhhhhh 😠😡😡🤬🤬🤬🫤🫨😵‍💫😵😪😪😪😪😪")
